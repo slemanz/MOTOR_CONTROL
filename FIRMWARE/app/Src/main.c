0f5a1e96df52c86128b05f33b66d17117690f547
slemanz <william_sleman@hotmail.com>
2024-11-02 08:57:59 -0300

    project(feat): add lcd lib

--- FIRMWARE/app/Src/main.c
+++ FIRMWARE/app/Src/main.c
@@ -1,5 +1,6 @@
 #include "stm32f401xx.h"
 #include "core/system.h"
+#include "core/lcd.h"
 
 
 // Function delay
@@ -14,6 +15,17 @@ int main(void)
  {
     system_setup();
     gpio_setup();
+    lcd_init();
+    lcd_set_cursor(1 , 1);
+    delay_cycles(4000000);
+    lcd_print_char('A');
+    delay_cycles(4000000);
+    lcd_display_clear();
+    lcd_display_return_home();
+    delay_cycles(4000000);
+    lcd_print_string((char*)"Sabadou!\0");
+
+    while(1) __asm("NOP");
 
     uint64_t start_time = system_get_ticks();
 
@@ -22,6 +34,7 @@ int main(void)
         if((system_get_ticks() - start_time) >= 1000)
         {
             GPIO_ToggleOutputPin(LED_PORT, LED_PIN);
+            lcd_print_char('b');
             start_time = system_get_ticks();
         }
 
